c4f4fd4eae6aa1d80094b78981143de989c4783d
Antonio Pelayo <aPelayo.py@gmail.com>
2020-09-17 19:29:52 -0700

    Add jQuery .slideDown animaiton to FAQ section
    Create ids: q and ans for FAQ question and answer animations

--- script.js
+++ script.js
@@ -26,3 +26,40 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2500); 
 }
+
+// FAQ Dropdown
+$("#q1").click(function() {
+    if ($("#ans1").is(":hidden")) {
+        $("#ans1").slideDown("slow");
+    } else {
+        $("#ans1").slideUp("slow");
+    };
+});
+$("#q2").click(function() {
+    if ($("#ans2").is(":hidden")) {
+        $("#ans2").slideDown("slow");
+    } else {
+        $("#ans2").slideUp("slow");
+    };
+});
+$("#q3").click(function() {
+    if ($("#ans3").is(":hidden")) {
+        $("#ans3").slideDown("slow");
+    } else {
+        $("#ans3").slideUp("slow");
+    };
+});
+$("#q4").click(function() {
+    if ($("#ans4").is(":hidden")) {
+        $("#ans4").slideDown("slow");
+    } else {
+        $("#ans4").slideUp("slow");
+    };
+});
+$("#q5").click(function() {
+    if ($("#ans5").is(":hidden")) {
+        $("#ans5").slideDown("slow");
+    } else {
+        $("#ans5").slideUp("slow");
+    };
+});
